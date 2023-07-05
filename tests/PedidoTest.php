c287e87b2efca487aed2d54b684f0bc35fa9f9a8
Wilson <wilsongome@gmail.com>
2023-07-05 12:17:02 -0300

    Pedido Teste

--- tests/PedidoTest.php
+++ tests/PedidoTest.php
@@ -6,20 +6,20 @@ use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
-class ProdutoTest extends TestCase
+class PedidoTest extends TestCase
 {
     use DatabaseTransactions;
 
     public function test_update_request_ok(): void
     {
-        $response = $this->call('PUT', '/produto/1', ['nome' => 'Produto teste']);
+        $response = $this->call('PUT', '/pedido/1', ['ativo' => 1]);
         $status = $response->status();
         $this->assertContains($status, array(200, 404), "HTTP status code must be 200 or 404");
     }
 
     public function test_update_request_error(): void
     {
-        $response = $this->call('PUT', '/produto/1', ['ativo' => 'aaa']);
+        $response = $this->call('PUT', '/pedido/1', ['ativo' => 'aaa']);
         $this->assertResponseStatus(422, "HTTP status code must be 422.");
     }
 
@@ -27,49 +27,47 @@ class ProdutoTest extends TestCase
     //Este método está comentado porque não está funcionando. Problemas com a lib GD
     //Para não parar o projeto por conta disso, comentei e segui sem ele
     //Não consegui testar o upload de imagem
-    /* public function test_store_request_ok(): void
+    public function test_store_request_ok(): void
     {
         $response = $this->post(
-            '/produto', 
-            [
-                'nome' => 'Produto teste', 
-                'preco' => '9,90',
-                'foto' =>  $file = UploadedFile::fake()->image('produto.jpg'),
-                'tipo_produto' => '1'
-            ]);
+            '/pedido', [
+                'cliente_id' => 1,
+                'produtos' => '[1, 2, 3]'
+                ]
+        );
         $this->assertResponseStatus(200, "HTTP status code must be 200.");
-    } */
+    }
 
     public function test_store_request_error(): void
     {
-        $response = $this->post('/produto', ['nome' => 'Produto teste', 'ativo' => 'aaa']);
+        $response = $this->post('/pedido', ['nome' => 'Produto teste', 'ativo' => 'aaa']);
         $this->assertResponseStatus(422, "HTTP status code must be 422.");
     }
 
     public function test_index_request_ok(): void
     {
-        $response = $this->call('GET','/produtos');
+        $response = $this->call('GET','/pedidos');
         $status = $response->status();
         $this->assertContains($status, array(200, 404), "HTTP status code must be 200 or 404");
     }
 
     public function test_index_request_error(): void
     {
-        $response = $this->call('GET','/produtosS');
+        $response = $this->call('GET','/pedidosS');
         $status = $response->status();
         $this->assertContains($status, array(422, 404, 503), "HTTP status code must be 422, 404, 503");
     }
 
     public function test_show_request_ok(): void
     {
-        $response = $this->call('GET','/produto/1');
+        $response = $this->call('GET','/pedido/1');
         $status = $response->status();
         $this->assertContains($status, array(200, 404), "HTTP status code must be 200 or 404");
     }
 
     public function test_show_request_error(): void
     {
-        $response = $this->call('POST','/produto/1');
+        $response = $this->call('POST','/pedido/1');
         $status = $response->status();
         $this->assertContains($status, array(405), "HTTP status code must be 405");
     }
